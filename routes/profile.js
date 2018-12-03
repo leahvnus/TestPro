a7c29baee9469f1d89807c562fa19c0d7f47bce3
piyatida paenghom <58030190@kmitl.ac.th>
2018-12-03 14:20:06 +0700

    14.19
    - update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -3,14 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    ab1.find({}, function(err, showme) {
-            if (err) {
-                return next(err);
-            } else {
-                //res.json(showab);
-                res.render('profile', { title: 'ข้อมูลส่วนตัว', data: showme })
-            }
-        })
-        // res.render('showdb', { title: 'ShowDB' });
+    res.render('profile', { title: 'ข้อมูลส่วนตัว' })
 });
 module.exports = router;
